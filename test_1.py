5a8991bf63666ea66108e29d36b947f2eb7d66c1
tomaszsito <tomasz.sito.666@gmail.com>
2023-08-27 06:25:22 +0200

    Adding 3rd example of comprehension

--- test_1.py
+++ test_1.py
@@ -9,5 +9,5 @@ print({key: value for key, value in enumerate(range(100, 0, -2))})
 print(len({x*y for x in range(1, 11) for y in range(1, 11)}))
 
 # comprehension #3
-# sequant power for 2
+# sequant power for number 2
 print([2**i for i in range(65)])
