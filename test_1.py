97d0e71f66afeb6972c17deb7adc14be0b6f58d7
tomaszsito <tomasz.sito.666@gmail.com>
2023-08-27 06:24:10 +0200

    + 3rd example of comprehension

--- test_1.py
+++ test_1.py
@@ -1,7 +1,13 @@
 # just test of GIT, nothing else
 
 # comprehension #1
+# generator of pairs: index and integer from specified range
 print({key: value for key, value in enumerate(range(100, 0, -2))})
 
 # comprehension #2
-print(len({x*y for x in range(1,11) for y in range(1,11)}))
+# number of unique numbers as result of multiplication up to 100
+print(len({x*y for x in range(1, 11) for y in range(1, 11)}))
+
+# comprehension #3
+# sequant power for 2
+print([2**i for i in range(65)])
