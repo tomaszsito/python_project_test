e6b9982a2911ccf455aa859e2b41b3aa5ec2a046
tomaszsito <tomasz.sito.666@gmail.com>
2023-08-27 06:16:16 +0200

    + 1 example of comprehension

--- test_1.py
+++ test_1.py
@@ -1,3 +1,7 @@
 # just test of GIT, nothing else
 
+# comprehension #1
 print({key: value for key, value in enumerate(range(100, 0, -2))})
+
+# comprehension #2
+print(len({x*y for x in range(1,11) for y in range(1,11)}))
