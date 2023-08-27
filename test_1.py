51456f849328c54ef9a49bc244c979f882907cac
tomaszsito <tomasz.sito.666@gmail.com>
2023-08-27 08:04:24 +0200

    Adding comprehension #7 – using 'generator'

--- test_1.py
+++ test_1.py
@@ -9,20 +9,26 @@ print({key: value for key, value in enumerate(range(100, 0, -2))})
 print(len({x*y for x in range(1, 11) for y in range(1, 11)}))
 
 # comprehension #3
-# sequant power for number 2
+# power for number 2
 print([2**i for i in range(65)])
 
 # comprehension #4
 # results of multiplication all numbers in range 0 - 10
-print({x*y for x in range(1,11) for y in range(1,11)})
-print(len({x*y for x in range(1,11) for y in range(1,11)}))
+print({x*y for x in range(1, 11) for y in range(1, 11)})
+print(len({x*y for x in range(1, 11) for y in range(1, 11)}))
 
 # comprehension #5
 # results of multiplication all different numbers in range 0 - 10
-print({x*y for x in range(1,11) for y in range(1,11) if x != y})
-print(len({x*y for x in range(1,11) for y in range(1,11) if x != y}))
+print({x*y for x in range(1, 11) for y in range(1, 11) if x != y})
+print(len({x*y for x in range(1, 11) for y in range(1, 11) if x != y}))
 
 # comprehension #6
 # dict – key: value
 print({x: x*x for x in range(-5, 6)})
 print(len({x: x*x for x in range(-5, 6)}))
+
+# comprehension #7
+# () – not tuple! Just generator.
+generator = (x*x for x in range(-3, 3))
+print(generator, type(generator))
+print(tuple(generator))
